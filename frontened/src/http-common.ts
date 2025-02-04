ab4cd05e310e748f3b66d514f31550f79541b8a8
smofktn <meijifj0215@gmail.com>
2025-02-05 01:59:17 +0900

    [add]axiosの共通設定ファイルを作成(#10)

---
+++ frontened/src/http-common.ts
@@ -0,0 +1,12 @@
+import axios, { type AxiosInstance } from "axios";
+
+const apiClient: AxiosInstance = axios.create({
+  // APIのURI
+  baseURL: "http://localhost:18080", //TODO:環境変数を使用する
+  // リクエストヘッダ
+  headers: {
+    "Content-type": "application/json",
+  },
+});
+
+export default apiClient;
