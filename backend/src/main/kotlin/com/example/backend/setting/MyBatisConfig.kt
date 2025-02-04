66e517ea3c66aa45dd8228f2f8a161624c784b0c
smofktn <meijifj0215@gmail.com>
2025-02-04 23:10:57 +0900

    [add]MyBatisConfigを追加(#12)

---
+++ backend/src/main/kotlin/com/example/backend/setting/MyBatisConfig.kt
@@ -0,0 +1,28 @@
+package com.example.backend.setting
+
+import org.mybatis.spring.SqlSessionFactoryBean
+import org.mybatis.spring.annotation.MapperScan
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.DefaultResourceLoader
+import org.springframework.core.io.support.ResourcePatternUtils
+import java.io.IOException
+import javax.sql.DataSource
+
+@Configuration
+@MapperScan("com.example.backend.i_mapper")
+class MyBatisConfig {
+    @Bean
+    @Throws(IOException::class)
+    fun sqlSessionFactoryBean(dataSource: DataSource?): SqlSessionFactoryBean {
+        val factory = SqlSessionFactoryBean()
+        factory.setDataSource(dataSource)
+
+        val resolver = ResourcePatternUtils.getResourcePatternResolver(DefaultResourceLoader())
+
+        // MyBatis のコンフィグレーションファイル
+        factory.setConfigLocation(resolver.getResource("classpath:mybatis-config.xml"))
+
+        return factory
+    }
+}
