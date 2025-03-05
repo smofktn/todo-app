83861e35c55aeea664eec360376dcb49ff48561d
smofktn <meijifj0215@gmail.com>
2025-03-05 21:30:53 +0900

    [add]変数定義を追加(#28)

---
+++ infrastructure/variables.tf
@@ -0,0 +1,39 @@
+variable "aws_profile" {
+  description = "aws profile"
+  type        = string
+  default     = "private"
+}
+# VPC
+variable "vpc_cidr" {
+  description = "The CIDR block of the VPC"
+  type        = string
+  default     = "10.0.0.0/24"
+}
+variable "vpc_name" {
+  type    = string
+  default = "practice-vpc"
+}
+# instance
+variable "instance_name" {
+  type    = string
+  default = "practice-server"
+}
+# subnet
+variable "vpc_subnet" {
+  type    = string
+  default = "10.0.0.0/24"
+}
+variable "vpc_subnet_name" {
+  type    = string
+  default = "practice-subnet"
+}
+# インターネットゲートウェイ
+variable "aws_internet_gateway_name" {
+  type    = string
+  default = "practice-igw"
+}
+# パブリックルートテーブル
+variable "root_table_public_table" {
+  type    = string
+  default = "practice-root-table-public"
+}
